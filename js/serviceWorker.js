278640cea91c6fc51cd0f177e2079663193012e0
Costin Andronache <Costin.Andronache@endava.com>
2020-10-20 17:19:28 +0300

    Update :: making the sw more generic

--- js/serviceWorker.js
+++ js/serviceWorker.js
@@ -1,4 +1,4 @@
-const pwaCACHE = "pwaCACHE";
+const offline = "pwaCACHE";
 const assets = [
   "/",
   "index.html",
@@ -12,18 +12,28 @@ const assets = [
   "resources/csharp.txt"
 ];
 
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(pwaCACHE).then(cache => {
-      cache.addAll(assets);
-    })
-  );
+// install trigger -> cache index.html
+self.addEventListener('install', function(event) {
+  var indexPage = new Request('/');
+  event.waitUntil(
+    fetch(indexPage).then(function(response) {
+      return caches.open(offline).then(function(cache) {
+        return cache.put(indexPage, response);
+      });
+  }));
 });
-
-self.addEventListener("fetch", fetchEvent => {
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request);
+// fetch trigger -> fetch from server, save to cache, if fail: serve from cache
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).then(function(response) {
+      return caches.open(offline).then(function(cache) {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    }).catch(function (error) {
+      return caches.open(offline).then(function(cache) {
+        return cache.match(event.request);
+      });
     })
   );
 });
