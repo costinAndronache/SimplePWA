f6ad827e831af2fe8baa8ab99f6e4d2447de2cff
Costin Andronache <Costin.Andronache@endava.com>
2020-10-20 15:48:42 +0300

    First steps

---
+++ js/main.js
@@ -0,0 +1,141 @@
+const data = [
+  {
+    name: "C++",
+    image: "resources/cpp.png",
+    doc: "resources/cpp.txt"
+  },
+
+  { 
+    name: "Swift",
+    image: "resources/swift.jpg",
+    doc: "resources/swift.txt"
+  },
+  {
+    name: "C#",
+    image: "resources/csharp.png",
+    doc: "resources/csharp.txt"
+  }
+];
+
+
+class Card {
+
+  constructor(name, imageSource, callback) {
+    this.imageSource = imageSource;
+    this.name = name;
+    this.callback = callback;
+  }
+
+  divElement() {
+    var mainCard = document.createElement("div");
+    mainCard.setAttribute("class", "card");
+
+    var img = document.createElement("img");
+    img.setAttribute("src", this.imageSource);
+    img.setAttribute("class", "card--image");
+
+    var h1 = document.createElement("h1");
+    h1.setAttribute("class", "card--title");
+    h1.innerHTML = this.name;
+
+    mainCard.appendChild(img);
+    mainCard.appendChild(h1);
+    mainCard.addEventListener("click", this.callback);
+
+    return mainCard
+  }
+
+}
+
+class ResourceLoader {
+
+  constructor(path) {
+    this.path = path;
+  }
+
+  load(callback) {
+
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
+           if (xmlhttp.status == 200) {
+               callback(xmlhttp.responseText);
+           }
+           else if (xmlhttp.status == 400) {
+              alert('There was an error 400');
+           }
+           else {
+               alert('something else other than 200 was returned');
+           }
+        }
+    };
+
+    xmlhttp.open("GET", this.path, true);
+    xmlhttp.send();
+
+  }
+
+}
+
+class Overlay {
+
+  constructor() {
+    var mainOverlay = document.createElement("div");
+    mainOverlay.setAttribute("id", "overlay");
+
+    var overlayText = document.createElement("div");
+    overlayText.setAttribute("id", "overlay-text");
+    mainOverlay.appendChild(overlayText);
+    var self = this; 
+    mainOverlay.addEventListener("click", function() { self.hide(); });
+
+    this.mainOverlay = mainOverlay;
+    this.overlayText = overlayText;
+
+  }
+
+  showInside(element, text) {
+    element.appendChild(this.mainOverlay);
+    this.overlayText.innerHTML = text;
+    this.mainOverlay.style.display = "block";
+  }
+
+  hide() {
+    this.mainOverlay.parentNode.removeChild(this.mainOverlay);
+  }
+
+} 
+
+function installServiceWorker() {
+  if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("js/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err));
+  });
+}
+}
+
+function main() {
+  const overlay = new Overlay();
+  const container = document.querySelector(".container");
+
+  data.forEach((val,index) => {
+    const card = new Card(val.name, val.image, function() {
+      const rl = new ResourceLoader(val.doc);
+      rl.load(function(text) {
+         overlay.showInside(container, text);
+      });
+
+    });
+
+    container.appendChild(card.divElement());
+
+  });
+  
+  installServiceWorker();
+}
+
+document.addEventListener("DOMContentLoaded", main);
---
+++ js/serviceWorker.js
@@ -0,0 +1,29 @@
+const pwaCACHE = "pwaCACHE";
+const assets = [
+  "/",
+  "/index.html",
+  "/style/style.css",
+  "/js/main.js",
+  "/resources/cpp.png",
+  "/resources/csharp.png",
+  "/resources/swift.jpg",
+  "/resources/cpp.txt",
+  "/resources/swift.txt",
+  "/resources/csharp.txt"
+];
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(pwaCACHE).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
